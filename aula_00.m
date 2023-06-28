3c29cfede25f116485edd16c23eb907300fec86c
sxjosue <99757018+josuesxz@users.noreply.github.com>
2023-06-28 17:22:23 -0400

    aula no dia 28.06.2023

---
+++ aula_00.m
@@ -0,0 +1,16 @@
+clc;
+clear;
+
+x = input("Escreva um número: ");
+y = input("Escreva outro número");
+
+if x < y
+   resultado = (x/y);
+   disp(resultado)
+else 
+    resultado = (y/x);
+    disp(resultado)
+end
+
+
+
---
+++ aula_01.m
@@ -0,0 +1,9 @@
+clc;
+clear;
+
+a = input("Digite o primeiro termo: "); %primeiro termo
+b = input("Digite o último termo: "); %último termo da soma
+
+soma = ((a+b)*97)/2;  %soma dos termos de 97 valores a famosa soma de gauss
+
+disp(soma) %resultado final da soma de Gauss 
