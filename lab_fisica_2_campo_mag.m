b004ae1aedd3eb518f88163503e5000d46d51e11
Josué Souza <99757018+josuesxz@users.noreply.github.com>
2023-07-11 00:04:28 -0300

    programinha para a atividade né 10.07

---
+++ lab_fisica_2_campo_mag.m
@@ -0,0 +1,32 @@
+clc;
+clear;
+format shortE; %deixa as saídas em notação científica
+syms x
+
+mu_0 = 4*1e-7; % Valor da constante magnética em T·m/A
+n = 25; %numero de espiras
+largura = 23.52941;
+
+corrente = [0.05 0.12 0.14 0.17 0.23]; %valores da corrente no experimento
+theta = [20 30 38 41 50]*pi/180; %converte o vetor graus para radianos
+
+% CÁLCULO DO CAMPO MAGNÉTICO
+campo_magnetico = (sqrt(2)*mu_0*n*corrente)*(largura)
+tg_theta = tan(theta)
+
+% GRÁFICO B X TAN(THETA)
+plot(tg_theta, campo_magnetico, 'o-');
+xlabel('Campo magnetico');
+ylabel('Tangente de angulo');
+title('Gráfico B x tan(theta)');
+
+%REGRESSÃO LINEAR 
+coeficientes = polyfit(campo_magnetico, tg_theta, 1); % Ajusta uma curva polinomial de grau 1 (regressão linear)
+a = coeficientes(1); % Coeficiente angular (inclinação)
+b = coeficientes(2); % Coeficiente linear (intercepto)
+tendencia = a * x + b; % Valores previstos pela regressão
+pretty(simplify(tendencia))
+disp('Coeficiente angular (inclinação):');
+disp(a);
+disp('Coeficiente linear (intercepto):');
+disp(b);
