708975814d4ae769119bff96a8d90b4e8de76082
sxjosue <133665545+sxjosue@users.noreply.github.com>
2023-08-16 11:28:56 -0400

    atualizacao agora com margem de ganho e fase, zeta, frequencia natural, inversa delaplace e comparando P e PID

--- Atividade de laboratorio de controle/Atividade_laboratorio_controle.m
+++ Atividade de laboratorio de controle/Atividade_laboratorio_controle.m
@@ -1,5 +1,6 @@
 clc;
 clear;
+syms s t;
 
 %DEFINDO A FUNÇÃO DE TRANSFERÊNCIA
 num = 1;
@@ -12,10 +13,26 @@ p1.OutputDelay = h; %preparando o valor de atraso
 p = series(p1, k); %sistema com um ganho 'k' qualquer
 g = feedback(p, 1); %sistema sendo realimentado 
 
+%REVERSO DE LAPLACE
+[N, D] = tfdata(p1, 'v'); %parte sem delay
+[R, P, K] = residue(N, D);
+
+fSemdelay = ilaplace(poly2sym(num, s) / poly2sym(den, s)); %funcao sem dalay
+fComdelay = subs(fSemdelay, t, t-h) * heaviside(t - h); %funcao com delay para incorporar
+
+disp('Inversa de Laplace:');
+pretty(fComdelay);
+
 %ESPAÇO DE ESTADOS
 [A, B, C, D] = tf2ss(num, den); %converter FT -> Espaço estados 
 sistema_ss = ss(A, B, C, D);
 
+% PARÂMETROS BÁSICOS DA RESPOSTA TEMPORAL
+p = pole(g);
+w = abs(p(1)); 
+zeta = -real(p(1)) / w;
+disp(['zeta = ', num2str(zeta)]);
+disp(['w = ', num2str(w), ' rad/s']);
 
 %RESPOSTA TEMPOAL A UM DEGRAU UNITÁRIO
 info = stepinfo(g);
@@ -31,6 +48,12 @@ sprintf('Raiz = %.2f ', raiz)
 Kp = dcgain(g); %calculo de erro para degrau    
 erro_estacionario = 1 / (1 + Kp)
 
+%MARGEM DE GANHO E MARGEM DEFASE
+[GM, PM, wcg, wcp] = margin(g);
+disp(['Margem de Ganho: ', num2str(GM), ' dB']);
+disp(['Margem de Fase: ', num2str(PM), ' graus']); %inf(graus) nunca cruza o eixo
+disp(['Frequência de ganho 0db: ', num2str(wcg), ' rad/s']);
+disp(['Frequência de fase -180: ', num2str(wcp), ' rad/s']);
 
 %RESPOSTA DOS GRAFICOS 
 %step(g);
