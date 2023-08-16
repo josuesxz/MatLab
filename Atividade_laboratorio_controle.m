b2d3a8a67cf6abd60ed069b118ca2d2aff038161
Josué Souza <99757018+josuesxz@users.noreply.github.com>
2023-08-15 23:45:40 -0300

    desenvolvimento ok de relatório de lab de controle
    - Resposta Transitória
    - Espaços de Estados
    - Análise em frequência

--- Atividade_laboratorio_controle.m
+++ Atividade_laboratorio_controle.m
@@ -1,24 +1,41 @@
 clc;
 clear;
 
+%DEFINDO A FUNÇÃO DE TRANSFERÊNCIA
 num = exp(-1);
 den = [1 3 1];
-k = 40;
+k = 1; %definido um 'k' qualquer
 
 p1 = tf(num, den); %sistema de segunda ordem
 p = series(p1, k); %sistema com um ganho 'k' qualquer
-g = feedback(p, 1) %sistema sendo realimentado 
+g = feedback(p, 1); %sistema sendo realimentado 
 
-%informações da resposta temporal a um degrau
+%ESPAÇO DE ESTADOS
+[A, B, C, D] = tf2ss(num, den); %converter FT -> Espaço estados 
+sistema_ss = ss(A, B, C, D);
+
+%RESPOSTA TEMPOAL A UM DEGRAU UNITÁRIO
 info = stepinfo(g);
 sprintf('Tr =  %.2f s', info.RiseTime) %tempo de subida
 sprintf('Ts = %.2f s', info.SettlingTime) %tempo de acomodação 
 sprintf('MP = %.2f%%', info.Overshoot) %sobresinal
 sprintf('Tp = %.2f s', info.PeakTime) %tempo de pico
 
-%Resposta de gráficos
-step(g);
+raiz = conv(den, num); %raízes do polonimio
+sprintf('Raiz = %.2f ', raiz)
+
+%ERRO ESTACIONÁRIO
+Kp = dcgain(g); %calculo de erro para degrau    
+erro_estacionario = 1 / (1 + Kp);
+
+
+%RESPOSTA DOS GRAFICOS 
+%step(g);
 %rlocus(g);
 %bode(g);
 %nyquist(g);
+%sisotool(g); %mostra todo os resultados acima 
+%pzmap(g); %mapa de polos e zeros
+
+
 
